2a8b1a806be7f9e32cb2ee0a8ab5758e663abaae
BhagatHarsh <93080554+BhagatHarsh@users.noreply.github.com>
2023-09-18 16:06:49 +0000

    Testing some changes
    checking if this ping actually registers on my github as a view or not

--- ping.js
+++ ping.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const app = express();
 
 app.get('/', (req, res) => {
-    axios.get('https://cpbot.onrender.com')
+    axios.get('https://github.com/BhagatHarsh')
     .then(response => {
         console.log('Ping success:', response.status);
     })
